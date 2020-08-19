30c122b9bea9c73f0c50609a8ae8ba227b0d6ac7
ws.park <ws.park@solarwiz.co.kr>
2020-08-20 08:07:31 +0900

    Updates

---
+++ precache-manifest.4e310a6917b15f014b6a8d97c7508f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e6cbc8d6cc4a409936d68cb25389f2",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c90a9373cbed7873f633",
+    "url": "/movie_app_2020/static/css/main.43c8b52e.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90a9373cbed7873f633",
+    "url": "/movie_app_2020/static/js/main.2703b120.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
