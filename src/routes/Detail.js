4e0900920b197390fb37c2bb11d292716e49a971
ws.park <ws.park@solarwiz.co.kr>
2020-08-20 10:09:38 +0900

    update

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,7 +10,27 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <>
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="movie__data">
+            <h3 className="movie__title">{location.state.title}</h3>
+            <h5 className="movie__year">{location.state.year}</h5>
+            <ul className="movie__genres">
+              {location.state.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{location.state.summary}</p>
+          </div>
+        </>
+      );
     } else {
       return null;
     }
